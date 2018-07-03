1f767950785c583915fd157243c8719735946547
ArtGluhovskiy <gluhovskiy.art@gmail.com>
2018-07-03 21:53:48 +0300

    Java Web Labs: Profiling Bean Post Processor (JMX MBean ProfilingConfig was added).

---
+++ src/main/java/org/art/java_web/labs/jmx/ProfilingConfig.java
@@ -0,0 +1,21 @@
+package org.art.java_web.labs.jmx;
+
+public class ProfilingConfig implements ProfilingConfigMBean {
+
+    private boolean enabledProfiling = true;
+
+    @Override
+    public void setEnabledProfiling(boolean enabledProfiling) {
+        this.enabledProfiling = enabledProfiling;
+    }
+
+    @Override
+    public boolean isEnabledProfiling() {
+        return enabledProfiling;
+    }
+
+    @Override
+    public void printStatus() {
+        System.out.println("Profiling config enabled: " + enabledProfiling);
+    }
+}
--- src/main/java/org/art/java_web/labs/web/bean_post_processors/ProfilingAnnotationBeanPostProcessor.java
+++ src/main/java/org/art/java_web/labs/web/bean_post_processors/ProfilingAnnotationBeanPostProcessor.java
@@ -2,11 +2,15 @@ package org.art.java_web.labs.web.bean_post_processors;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.art.java_web.labs.jmx.ProfilingConfig;
 import org.art.java_web.labs.web.annotations.Profiling;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.util.ReflectionUtils;
 
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.HashMap;
@@ -19,13 +23,25 @@ import java.util.Set;
  * that checks if a bean or some bean's method is annotated with {@link Profiling}
  * annotation. If so, creates dynamic JDK proxy which intercepts invoked method and
  * measures elapsed method invocation time.
+ * Can be enabled/disabled via JMX {@link ProfilingConfig}.
  */
 public class ProfilingAnnotationBeanPostProcessor implements BeanPostProcessor {
 
     private static final Logger LOG = LogManager.getLogger(ProfilingAnnotationBeanPostProcessor.class);
 
+    private ProfilingConfig profilingConfig;
+
     private final Map<String, Class> targetBeanClasses = new HashMap<>();
+
     private final Map<String, Set<String>> targetBeanMethods = new HashMap<>();
+
+    public ProfilingAnnotationBeanPostProcessor() throws Exception {
+        //Profiling config JMX MBean registering
+        this.profilingConfig = new ProfilingConfig();
+        MBeanServer platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
+        ObjectName mBeanName = new ObjectName("org.art.java_web.labs.jmx:type=ProfilingConfig");
+        platformMBeanServer.registerMBean(profilingConfig, mBeanName);
+    }
 
     @Override
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
@@ -39,15 +55,15 @@ public class ProfilingAnnotationBeanPostProcessor implements BeanPostProcessor {
         }
         //Check if bean class has methods annotated with '@Profiling' annotation
         Method[] methods = beanClass.getDeclaredMethods();
-        Set<String> methodSet = new HashSet<>();
+        Set<String> targetMethods = new HashSet<>();
         for (Method method : methods) {
             targetAnnotation = method.getAnnotation(Profiling.class);
             if (targetAnnotation != null) {
-                methodSet.add(method.getName());
+                targetMethods.add(method.getName());
             }
         }
-        if (!methodSet.isEmpty()) {
-            targetBeanMethods.put(beanName, methodSet);
+        if (!targetMethods.isEmpty()) {
+            targetBeanMethods.put(beanName, targetMethods);
         }
         return bean;
     }
@@ -60,9 +76,8 @@ public class ProfilingAnnotationBeanPostProcessor implements BeanPostProcessor {
         } else if (targetBeanMethods.containsKey(beanName)) {
             Set<String> targetMethods = targetBeanMethods.get(beanName);
             return createMethodProfilingProxy(bean, beanName, targetMethods);
-        } else {
-            return bean;
         }
+        return bean;
     }
 
     private Object createClassProfilingProxy(Object bean, String beanName) {
@@ -76,18 +91,23 @@ public class ProfilingAnnotationBeanPostProcessor implements BeanPostProcessor {
                     if (targetMethods.contains(method.getName())) {
                         return invokeWithProfiling(bean, beanName, method, args);
                     } else {
-                        return method.invoke(bean, args);
+                        return ReflectionUtils.invokeMethod(method, bean, args);
                     }
                 });
     }
 
     private Object invokeWithProfiling(Object bean, String beanName, Method method, Object[] args) {
-        LOG.info("*** Start profiling. Bean {}: method name: {}", beanName, method.getName());
-        long start = System.nanoTime();
-        Object ret = ReflectionUtils.invokeMethod(method, bean, args);
-        long end = System.nanoTime();
-        LOG.info("*** End profiling. Bean {}: method name: {}. Elapsed time: {} mcs",
-                beanName, method.getName(), (end - start) / (long) 10e3);
+        Object ret;
+        if (profilingConfig.isEnabledProfiling()) {
+            LOG.info("*** Start profiling. Bean {}: method name: {}", beanName, method.getName());
+            long start = System.nanoTime();
+            ret = ReflectionUtils.invokeMethod(method, bean, args);
+            long end = System.nanoTime();
+            LOG.info("*** End profiling. Bean {}: method name: {}. Elapsed time: {} mcs",
+                    beanName, method.getName(), (end - start) / (long) 10e3);
+        } else {
+            ret = ReflectionUtils.invokeMethod(method, bean, args);
+        }
         return ret;
     }
 }
